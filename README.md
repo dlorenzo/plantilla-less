40cdc2c6fb81e9d934f6d48c1c34b0abafbb7432
Daniel Lorenzo <daniel.lorenzoabalo@plexus.es>
2017-08-09 13:33:44 +0200

    Versión inicial

---
+++ README.md
@@ -0,0 +1,14 @@
+# Plantilla LESS
+
+### v1.0-rc1
+
+## Instalación
+
+`npm install`
+
+
+## Uso
+
+`gulp watch` para compilar e iniciar control de cambios.
+
+`gulp build` para generar versión 'release'.
---
+++ gulpfile.js
@@ -0,0 +1,45 @@
+var gulp = require('gulp');
+var less = require('gulp-less');
+var wait = require('gulp-wait');
+var sourcemaps = require('gulp-sourcemaps');
+var path = require('path');
+var clean = require('gulp-clean');
+
+gulp.task('build', ['clean'], function () {
+  procLess(true);
+});
+
+gulp.task('watch', ['clean'], function () {
+  procSass();
+  gulp.watch('./less/{,**/}*.{less}', ['less']);
+});
+
+gulp.task('clean', function () {
+  return gulp.src('./css/*', { read: false })
+    .pipe(clean());
+});
+
+gulp.task('less', procLess);
+
+/**
+ * Estilos
+ */
+function procLess(release = false) {
+  if (release === true) {
+    return gulp.src('./less/**/*.less')
+      .pipe(wait(500))
+      .pipe(less({
+         paths: [ path.join(__dirname, 'less', 'includes') ]
+      }))
+      .pipe(gulp.dest('./css'));
+  } else {
+    return gulp.src('./scss/**/*.scss')
+      .pipe(wait(1000))
+      .pipe(sourcemaps.init())
+      .pipe(less({
+         paths: [ path.join(__dirname, 'less', 'includes') ]
+      }))
+      .pipe(sourcemaps.write())
+      .pipe(gulp.dest('./css'));
+  }
+}
